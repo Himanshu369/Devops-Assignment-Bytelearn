867accfdc5901422ab898c66df669400f6334c8d
HimanshuV <himanshu_v@quartic.ai>
2022-05-20 02:08:34 +0530

    update changes

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, jsonify
-import os
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return jsonify('hello world')
-
-prot = os.environ.get("PORT", 5000)
-app.run(debug=False, host="0.0.0.0", port=port)
